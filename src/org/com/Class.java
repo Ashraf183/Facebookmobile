62ba9e23ce3d0442de1036d006a9fb0dc5f29bbc
Ashraf183 <ashvk183@gmail.com>
2021-12-31 16:59:43 +0530

    password funtionality

--- src/org/com/Class.java
+++ src/org/com/Class.java
@@ -1,20 +1,18 @@
 package org.com;
 
+
+
 public class Class {
-	
-	
-	
-	
 	
 	public static void password() {
 		System.out.println("password");
-		
-	
-	
-	
 	}
 	public static void main(String[] args) {
 		password();
 	}
 
 }
+	
+	
+	
+	
